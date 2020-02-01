7185fb1f724d106e0e1c3731b3acd64ef810947c
Sean Bittner <seanrbittner@gmail>
2020-02-01 11:46:25 -0500

    commented error formatters

--- epi/error_formatters.py
+++ epi/error_formatters.py
@@ -2,6 +2,17 @@
 
 
 def format_type_err_msg(obj, arg_name: str, arg, correct_type) -> str:
+    """Formats error message for incorrect types.
+
+    :param obj: The function or class to which argument was supplied.
+    :type obj: object
+    :param arg_name: Name of function argument.
+    :type arg_name: str
+    :param arg: The argument with incorrect type.
+    :param correct_type: The correct type of the argument.
+    :return: Type error message.
+    :rtype: str
+    """
     arg_type = arg.__class__
     if arg_type is correct_type:
         raise ValueError("Invalid TypeError message: type(arg) == correct_type.")
