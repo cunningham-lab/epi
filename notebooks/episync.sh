dd7b6760c5ad3dfa59c1367d5697c1d1f35a0927
Sean Bittner <seanrbittner@gmail.com>
2020-10-06 21:47:25 -0400

    updating sync file

--- notebooks/episync.sh
+++ notebooks/episync.sh
@@ -1,1 +1,2 @@
 rsync -azhe ssh --progress srb2201@habanero.rcs.columbia.edu:/rigel/stats/users/srb2201/epi/scripts/$1/data $1/
+rsync -azhe ssh --progress srb2201@terremoto.rcs.columbia.edu:/moto/stats/users/srb2201/epi/scripts/$1/data $1/
