ddfcca055904cb34d81c0836f5d40abd5d40af5b
Sean Bittner <seanrbittner@gmail.com>
2020-10-15 11:52:27 -0400

    dvdh

--- scripts/SSSN/dvdh_epi.py
+++ scripts/SSSN/dvdh_epi.py
@@ -8,6 +8,7 @@ import time
 
 # Parse script command-line parameters.
 parser = argparse.ArgumentParser()
+parser.add_argument('--contrast', type=float, default=0.) # aug lag hp
 parser.add_argument('--alpha', type=str, default='E') # neuron type
 parser.add_argument('--beta', type=float, default=4.) # aug lag hp
 parser.add_argument('--logc0', type=float, default=0.) # log10 of c_0
@@ -15,6 +16,7 @@ parser.add_argument('--bnmom', type=float, default=.99) # log10 of c_0
 parser.add_argument('--random_seed', type=int, default=1)
 args = parser.parse_args()
 
+contrast = args.contrast
 alpha = args.alpha
 beta = args.beta
 c0 = 10.**args.logc0
@@ -24,7 +26,7 @@ random_seed = args.random_seed
 npzfile = np.load('data/SV_mode.npz')
 z_mode1 = npzfile['z_mode1'][0]
 DH = z_mode1[4]
-H = z_mode1[:4] + DH*np.array([1., 1., 0., 0.])
+H = z_mode1[:4] + contrast*DH*np.array([1., 1., 0., 0.])
 
 neuron_inds = {'E':0, 'P':1, 'S':2, 'V':3}
 neuron_ind = neuron_inds[alpha]
@@ -54,7 +56,7 @@ ub = 10.*np.ones((D,))
 dh = Parameter("dh", D, lb=lb, ub=ub)
 
 # Define model
-name = "SSSN_drdh_%s" % alpha
+name = "SSSN_dvdh_%s_c=%.1f" % (alpha, contrast)
 parameters = [dh]
 model = Model(name, parameters)
 
@@ -66,7 +68,7 @@ v_rest = -70.
 
 dt = 0.005
 
-N = 5
+N = 1
 T = 100
 
 def f_r(v):
@@ -103,21 +105,21 @@ def SSSN_sim(h):
     v_ss = euler_sim_stoch(f, y_init, dt, T)
     return v_ss
 
-def dr(dh):
+def dv(dh):
     h = tf.constant(H[None,:], dtype=tf.float32)
 
     x1 = f_r(SSSN_sim(h)[:,:,neuron_ind])
     x2 = f_r(SSSN_sim(h + dh)[:,:,neuron_ind])
 
-    diff = tf.reduce_mean(x2 - x1, axis=1, keepdims=True)
+    diff = x2 - x1
     T_x = tf.concat((diff, diff ** 2), axis=1)
 
     return T_x
 
-model.set_eps(dr)
+model.set_eps(dv)
 
 # Emergent property values.
-mu = np.array([0., 1.**2])
+mu = np.array([0., 1.])
 
 # 3. Run EPI.
 q_theta, opt_data, epi_path, failed = model.epi(
