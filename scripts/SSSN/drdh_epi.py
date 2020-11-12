53d4021ebf69c120efe64dd39a2ae74ae82bc5b0
Sean Bittner <seanrbittner@gmail.com>
2020-11-12 16:28:40 -0500

    epi sssn eps

--- scripts/SSSN/drdh_epi.py
+++ scripts/SSSN/drdh_epi.py
@@ -11,7 +11,7 @@ parser.add_argument('--contrast', type=float, default=0.) # aug lag hp
 parser.add_argument('--alpha', type=str, default='E') # neuron type
 parser.add_argument('--beta', type=float, default=4.) # aug lag hp
 parser.add_argument('--logc0', type=float, default=0.) # log10 of c_0
-parser.add_argument('--bnmom', type=float, default=.99) # log10 of c_0
+parser.add_argument('--eps', type=float, default=0.01) # log10 of c_0
 parser.add_argument('--random_seed', type=int, default=1)
 args = parser.parse_args()
 
@@ -19,7 +19,7 @@ contrast = args.contrast
 alpha = args.alpha
 beta = args.beta
 c0 = 10.**args.logc0
-bnmom = args.bnmom
+eps = args.eps
 random_seed = args.random_seed
 
 ind = 1070
@@ -31,6 +31,7 @@ neuron_inds = {'E':0, 'P':1, 'S':2, 'V':3}
 neuron_ind = neuron_inds[alpha]
 
 M = 500
+eps = 0.1
 
 # 1. Specify the V1 model for EPI.
 D = 4
@@ -40,13 +41,15 @@ ub = .25*np.ones((D,))
 dh = Parameter("dh", D, lb=lb, ub=ub)
 
 # Define model
-name = "SSSN_drdh_%s_c=%.1f" % (alpha, contrast)
+name = "SSSN_drdh_%s_c=%.1f_eps=%.3f" % (alpha, contrast, eps)
 parameters = [dh]
 model = Model(name, parameters)
 
+sssn_sim = SSSN_sim(eps)
+
 def dr(dh):
-    x1 = SSSN_sim(H)[:,:,neuron_ind]
-    x2 = SSSN_sim(H + dh)[:,:,neuron_ind]
+    x1 = sssn_sim(H)[:,:,neuron_ind]
+    x2 = sssn_sim(H + dh)[:,:,neuron_ind]
 
     diff = tf.reduce_mean(x2 - x1, axis=1)
     T_x = tf.stack((diff, diff ** 2), axis=1)
@@ -68,7 +71,7 @@ q_theta, opt_data, epi_path, failed = model.epi(
     num_units=50,
     post_affine=True,
     batch_norm=True,
-    bn_momentum=bnmom,
+    bn_momentum=0.99,
     K=15,
     N=M,
     num_iters=2500,
