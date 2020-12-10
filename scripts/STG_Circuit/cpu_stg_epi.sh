f820860a76daf2a94f2f8b52b1182fab66e483b8
Sean Bittner <seanrbittner@gmail.com>
2020-12-10 13:32:49 -0500

    terre scripting

--- scripts/STG_Circuit/cpu_stg_epi.sh
+++ scripts/STG_Circuit/cpu_stg_epi.sh
@@ -2,8 +2,8 @@
 #SBATCH --account=stats
 #SBATCH --job-name=STG_epi
 #SBATCH -c 1
-#SBATCH --time=3:00:00
+#SBATCH --time=11:00:00
 #SBATCH --mem-per-cpu=50gb
 
 source activate epi
-python3 stg_epi_fast.py --freq $1 --beta $2 --logc0 $3 --random_seed $4
+python3 stg_epi_fast.py --freq $1 --mu_std $2 --g_el_lb $3 --beta $4 --logc0 $5 --random_seed $6
