d649e1e6fdbbb53bb4553053cf8b2127829daba4
Sean Bittner <seanrbittner@gmail.com>
2021-03-09 16:19:05 -0500

    snpe cpu script

--- scripts/RNN/cpu_stab_amp_snpe.sh
+++ scripts/RNN/cpu_stab_amp_snpe.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account=stats
-#SBATCH --job-name=sa_snpe
+#SBATCH --job-name=rnn_snpe
 #SBATCH -c 1
-#SBATCH --time=11:30:00
+#SBATCH --time=2:30:00
 #SBATCH --mem-per-cpu=1gb
 
 source activate delfi
-python3 stab_amp_snpe.py --N $1 --n_train $2 --n_mades $3 --n_atoms $4 --rs $5
+python3 stab_amp_snpe.py --N $1 --n_train $2 --n_mades $3 --n_atoms $4 --g $5 --K $6 --rs $7
