1b83b7feedfdde07b9991eb1b35e0496765e2438
Sean Bittner <seanrbittner@gmail>
2021-03-13 13:19:57 -0500

    more logging for snpe

--- neural_circuits/LRRNN.py
+++ neural_circuits/LRRNN.py
@@ -3,12 +3,17 @@ import tensorflow as tf
 
 #EPS = 1e-10
 
-def get_W_eigs_np(g, K):
-    def W_eigs(U, V):
+def get_W_eigs_np(g, K, feed_noise=False):
+    def W_eigs(U, V, noise=None):
         U, V = U[None,:,:], V[None,:,:]
         U, V = np.tile(U, [K,1,1]), np.tile(V, [K,1,1])
-        U = U + g*np.random.normal(0., 1., U.shape)
-        V = V + g*np.random.normal(0., 1., V.shape)
+        if feed_noise:
+            U_noise, V_noise = noise
+            U = U + g*U_noise
+            V = V + g*V_noise
+        else:
+            U = U + g*np.random.normal(0., 1., U.shape)
+            V = V + g*np.random.normal(0., 1., V.shape)
         J = np.matmul(U, np.transpose(V, [0,2,1]))
         Js = (J + np.transpose(J, [0,2,1])) / 2.
         Js_eigs = np.linalg.eigvalsh(Js)
@@ -25,12 +30,39 @@ def get_W_eigs_np(g, K):
         return np.array([J_eig_realmax, Js_eig_max])
     return W_eigs
 
-def get_W_eigs_tf(g, K, Js_eig_max_mean=1.5, J_eig_realmax_mean=0.5):
-    def W_eigs(U, V):
+def get_W_eigs_full_np(g, K, feed_noise=False):
+    def W_eigs(U, V, noise=None):
+        J = np.matmul(U, np.transpose(V))[None, :, :]
+        J = np.tile(J, [K,1,1])
+        if feed_noise:
+            J = J + g*noise
+        else:
+            J = J + g*np.random.normal(0., 1., J.shape)
+        Js = (J + np.transpose(J, [0,2,1])) / 2.
+        Js_eigs = np.linalg.eigvalsh(Js)
+        Js_eig_maxs = np.max(Js_eigs, axis=1)
+        Js_eig_max = np.mean(Js_eig_maxs)
+       
+        J_eig_realmaxs = []
+        for k in range(K):
+            _J = J[k]
+            w,v = np.linalg.eig(_J)
+            J_eig_realmaxs.append(np.max(np.real(w)))
+        J_eig_realmax = np.mean(J_eig_realmaxs)
+        return np.array([J_eig_realmax, Js_eig_max])
+    return W_eigs
+
+def get_W_eigs_tf(g, K, Js_eig_max_mean=1.5, J_eig_realmax_mean=0.5, feed_noise=False):
+    def W_eigs(U, V, noise=None):
         U, V = U[:,None,:,:], V[:,None,:,:]
         U, V = tf.tile(U, [1,K,1,1]), tf.tile(V, [1,K,1,1])
-        U = U + g*tf.random.normal(U.shape, 0., 1.)
-        V = V + g*tf.random.normal(V.shape, 0., 1.)
+        if feed_noise:
+            U_noise, V_noise = noise
+            U = U + g*U_noise
+            V = V + g*V_noise
+        else:
+            U = U + g*tf.random.normal(U.shape, 0., 1.)
+            V = V + g*tf.random.normal(V.shape, 0., 1.)
         J = tf.matmul(U, tf.transpose(V, [0,1,3,2]))
         Js = (J + tf.transpose(J, [0,1,3,2])) / 2.
         Js_eigs = tf.linalg.eigvalsh(Js)
@@ -49,3 +81,30 @@ def get_W_eigs_tf(g, K, Js_eig_max_mean=1.5, J_eig_realmax_mean=0.5):
                         tf.square(Js_eig_max-Js_eig_max_mean)], axis=1)
         return T_x
     return W_eigs
+
+def get_W_eigs_full_tf(g, K, Js_eig_max_mean=1.5, J_eig_realmax_mean=0.5, feed_noise=False):
+    def W_eigs(U, V, noise=None):
+        J = tf.matmul(U, tf.transpose(V, [0,2,1]))
+        J = tf.tile(J[:,None,:,:], (1,K,1,1))
+        if feed_noise:
+            J = J + g*noise
+        else:
+            J = J + g*tf.random.normal(J.shape, 0., 1.)
+        Js = (J + tf.transpose(J, [0,1,3,2])) / 2.
+        Js_eigs = tf.linalg.eigvalsh(Js)
+        Js_eig_maxs = tf.reduce_max(Js_eigs, axis=2)
+        Js_eig_max = tf.reduce_mean(Js_eig_maxs, axis=1)
+
+        # Take eig of low rank similar mat
+        Jr = tf.matmul(tf.transpose(V, [0,2,1]), U) #+ EPS*tf.eye(2)[None,None,:,:]
+        Jr_tr = tf.linalg.trace(Jr)
+        maybe_complex_term = tf.sqrt(tf.complex(tf.square(Jr_tr) + -4.*tf.linalg.det(Jr), 0.))
+        J_eig_realmax = 0.5 * (Jr_tr + tf.math.real(maybe_complex_term))
+        J_eig_realmax = tf.math.maximum(J_eig_realmax, g)
+
+        T_x = tf.stack([J_eig_realmax, Js_eig_max,
+                        tf.square(J_eig_realmax-J_eig_realmax_mean),
+                        tf.square(Js_eig_max-Js_eig_max_mean)], axis=1)
+        return T_x
+    return W_eigs
+
--- scripts/RNN/stab_amp_snpe.py
+++ scripts/RNN/stab_amp_snpe.py
@@ -23,6 +23,9 @@ parser.add_argument('--num_transforms', type=int, default=3)
 parser.add_argument('--num_atoms', type=int, default=10)
 parser.add_argument('--g', type=float, default=0.01)
 parser.add_argument('--K', type=int, default=1)
+parser.add_argument('--max_rounds', type=int, default=50)
+parser.add_argument('--persist_rounds', type=int, default=5)
+parser.add_argument('--min_epochs', type=int, default=10)
 parser.add_argument('--rs', type=int, default=1)
 args = parser.parse_args()
 
@@ -33,6 +36,9 @@ num_transforms = args.num_transforms
 num_atoms = args.num_atoms
 g = args.g
 K = args.K
+max_rounds = args.max_rounds
+persist_rounds = args.persist_rounds
+min_epochs = args.min_epochs
 rs = args.rs
 torch.manual_seed(rs)
 
@@ -73,37 +79,61 @@ x_0 = torch.tensor([0.5, 1.5])
 
 inference = SNPE(prior, density_estimator=density_estimator_build_fun)
 
-max_rounds = 25
-persist_rounds = 5
 best_round = 0
 
 posteriors = []
 times = []
 proposal = prior
+round_val_log_probs = []
 for r in range(max_rounds):
     time1 = time.time()
     if r == 0:
-        print('Round %d:' % (r+1), flush=True)
+        print('Round %d/%d:' % (r+1, max_rounds), flush=True)
     else:
-        print('Round %d, Best (%d), Avg (%.1f min)):' % (r+1, best_round+1, np.mean(times)/60.), 
+        print('Round %d/%d, Best (%d), Avg (%.1f min)):' % (r+1, max_rounds, best_round+1, np.mean(times)/60.), 
                flush=True)
     theta, x = simulate_for_sbi(simulator, proposal=proposal, num_simulations=num_sims)
     inference = inference.append_simulations(theta, x)
-    density_estimator = inference.train(training_batch_size=num_batch, num_atoms=num_atoms, stop_after_epochs=10)
+    density_estimator = inference.train(
+        training_batch_size=num_batch, 
+        num_atoms=num_atoms, 
+        stop_after_epochs=min_epochs)
     posterior = inference.build_posterior(density_estimator)
     time2 = time.time()
     times.append(time2-time1)
     posteriors.append(posterior)
-    best_round = np.argmax(inference.summary['best_validation_log_probs'])
+    round_val_log_probs.append(inference.summary['validation_log_probs'][-1])
+    best_round = np.argmax(round_val_log_probs)
+    print('vlps', round_val_log_probs)
+    print('round', best_round)
     if best_round + persist_rounds == r:
         break
     proposal = posterior.set_default_x(x_0)
 
-zs = []
-for posterior in posteriors:
-    zs.append(posterior.sample((1000,), x=x_0).numpy())
+zs = [] 
+xs = []
+log_probs = [] 
+distances = []
+M = 1000
+for posterior in posteriors: 
+    z = posterior.sample((M,), x=x_0)
+    x = simulator(z).numpy()
+    log_prob = posterior.log_prob(z, x=x_0)
+    median_x = np.median(x, axis=0)
+    distance = np.linalg.norm(median_x - x_0.numpy())
 
-optim = {'summary':inference._summary, 'zs':np.array(zs), 'times':times}
+    zs.append(z.numpy())
+    xs.append(x)
+    log_probs.append(log_prob.numpy())
+    distances.append(distance)
+
+optim = {'summary':inference._summary, 
+         'round_val_log_probs':np.array(round_val_log_probs), 
+         'zs':np.array(zs), 
+         'xs':np.array(xs), 
+         'log_probs':np.array(log_probs),
+         'distances':np.array(distances),
+         'times':times}
 
 print('Saving', save_path, '...', flush=True)
 with open(os.path.join(save_path, "optim.pkl"), "wb") as f:
