726b2a94c122788d8d7ffc28b61b220287a1fedb
Sean Bittner <seanrbittner@gmail.com>
2021-03-18 10:37:56 -0400

    latest scripst

--- scripts/RNN/stab_amp_snpe_sweep.sh
+++ scripts/RNN/stab_amp_snpe_sweep.sh
@@ -2,22 +2,22 @@
 
 # This shell script does it linearly, but could you run each of these 
 # python scripts independently on different instances?
-for N in 5
+for N in 50 
 do
-  for g in 0.05 0.1 0.15
+  for g in 0.1
   do
     for K in 1
     do
-      for num_sims in 2000
+      for num_sims in 25000
       do
-        for num_batch in 100
+        for num_batch in 200 1000
         do
           for num_atoms in 100
           do
-            for rs in 1 2 3
+            for rs in 6
             do
               echo "Running N = $N, g = $g, K = $K, sims = $num_sims batch = $num_batch, atoms = $num_atoms, rs = $rs"
-              sbatch cpu_stab_amp_snpe.sh $N $num_sims $num_batch 3 $num_atoms $g $K $rs
+              sbatch gpu_stab_amp_snpe.sh $N $num_sims $num_batch 3 $num_atoms $g $K $rs
             done
           done
         done
