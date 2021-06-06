f35fe1bf380f5d502c82033a597c536f5a44d452
Sean Bittner <seanrbittner@gmail>
2021-06-06 15:26:13 -0400

    figure file rename

---
+++ written/eLifeRevision/figures/rename_figs.sh
@@ -0,0 +1,31 @@
+cp fig1/fig1.pdf fig1.pdf
+cp figLDS1/figLDS1.pdf fig1supp1.pdf
+cp figLDS2/figLDS2.pdf fig1supp2.pdf
+cp figLDS3/figLDS3.pdf fig1supp3.pdf
+cp figSTG1/figSTG1.pdf fig1supp4.pdf
+
+cp fig2/fig2.pdf fig2.pdf
+cp figRNN1/figRNN1.pdf fig2supp1.pdf
+cp figRNN2/figRNN2.pdf fig2supp3.pdf
+cp figRNN3/figRNN3.pdf fig2supp2.pdf
+
+cp fig3/fig3.pdf fig3.pdf
+cp figV1_1/figV1_1.pdf fig3supp4.pdf
+cp figV1_2/figV1_2.pdf fig3supp3.pdf
+cp figV1_3/figV1_3.pdf fig3supp1.pdf
+cp figV1_4/figV1_4.pdf fig3supp2.pdf
+
+cp fig4/fig4.pdf fig4.pdf
+cp figSC1/figSC1.pdf fig4supp1.pdf
+cp figSC2/figSC2.pdf fig4supp2.pdf
+cp figSC3/figSC3.pdf fig4supp3.pdf
+
+cp figSC5/figSC5.pdf fig5supp1.pdf
+cp figSC6/figSC6.pdf fig5supp2.pdf
+
+cp figSC7/figSC7.pdf fig4supp4.pdf
+cp figSC8/figSC8.pdf fig4supp5.pdf
+
+git add *.pdf
+#git commit -m "figure filename updates"
+
